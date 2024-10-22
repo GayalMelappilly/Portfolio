0a9345d2ded33f9f7f7183b75383658ba2fde7ac
Gayal Melappilly <gayalsunil@gmail.com>
2024-10-23 00:10:44 +0530

    Initial commit

---
+++ app/components/Sidepanel/Sidepanel.tsx
@@ -0,0 +1,15 @@
+import { poppins } from '@/app/layout'
+import React from 'react'
+import Button from '../Button/Button'
+
+type Props = {}
+
+const Sidepanel = (props: Props) => {
+    return (
+        <div className='flex bg-[#151515] shadow-[0_3px_10px_rgb(0,0,0,0.2)] h-[85%] w-[20%] rounded-md'>
+            <p>Gayal M Sunil</p>
+        </div>
+    )
+}
+
+export default Sidepanel
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,13 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Poppins } from 'next/font/google';
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+
+export const poppins = Poppins({
+  subsets: ['latin'], 
+  weight: ['400', '600', '700'], 
+  style: ['normal', 'italic'],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +22,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="bg-[#1E1E1E] h-screen">
         {children}
       </body>
     </html>
